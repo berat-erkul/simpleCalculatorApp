68feca367ea191b071c5b72857f15de615f0f265
berat-erkul <beraterkul00@gmail.com>
2024-02-23 01:31:03 +0300

    simple calculator app completed

--- simpleCalculatorApp/ViewController.swift
+++ simpleCalculatorApp/ViewController.swift
@@ -8,12 +8,89 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+        
+    
+    var result : Int = 0         //I defined a variable to use it inside the functions of the buttons
+    
+    
+    //I selected "outlet" when I was creating it.
+    @IBOutlet weak var firstTextField: UITextField!
+        
+    
+    //I selected "outlet" when I was creating it.
+    @IBOutlet weak var secondTextField: UITextField!
+        
+    
+    @IBOutlet weak var resultText: UILabel!
+    
+    
+    //sum button action defined
+    @IBAction func sumClicked(_ sender: Any) {
+                
+        if let firstNumber = Int(firstTextField.text!){     //control to get an integer from text field
+            
+            if let secondNumber = Int(secondTextField.text!){
+                
+                result = firstNumber + secondNumber
+                resultText.text = String(result)
+            }
+        }
+    }
+    
+    
+    
+    //minus button action defined
+    @IBAction func minusClicked(_ sender: Any) {
+        
+        if let firstNumber = Int(firstTextField.text!){     //control to get an integer from text field
+            
+            if let secondNumber = Int(secondTextField.text!){
+                
+                result = firstNumber - secondNumber
+                resultText.text = String(result)
+            }
+        }
+    }
+    
+    
+    
+    //multiply button action defined
+    @IBAction func multiplyClicked(_ sender: Any) {
+        
+        if let firstNumber = Int(firstTextField.text!){     //control to get an integer from text field
+            
+            if let secondNumber = Int(secondTextField.text!){
+                
+                result = firstNumber * secondNumber
+                resultText.text = String(result)
+            }
+        }
+    }
+    
+    
+    
+    //devide button action defined
+    @IBAction func devideClicked(_ sender: Any) {
+        
+        if let firstNumber = Int(firstTextField.text!){     //control to get an integer from text field
+            
+            if let secondNumber = Int(secondTextField.text!){
+                
+                result = firstNumber / secondNumber
+                resultText.text = String(result)
+            }
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
 }
-
